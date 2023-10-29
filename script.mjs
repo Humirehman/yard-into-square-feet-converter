ca6f8201a80e2d6c5226bba99a8d3e90fe2a0fc6
humirehman <humarehman429@gmail.com>
2023-10-29 03:35:45 -0700

    my changes

--- script.mjs
+++ script.mjs
@@ -1,10 +1,11 @@
-function convertyardToSquareFeet() {
-    // Get the value of yard input
+function doconvert(event) {
+    event.preventDefault();
+   
     var yard = parseFloat(document.getElementById("yard").value);
     
-    // Conversion factor: 1 yard is equal to 9 square feet
+   
     var squareFeet = yard * 9;
     
-    // Display the result on the screen
+   
     document.getElementById("result").innerHTML = yard + " yard = " + squareFeet + " Square Feet.";
 }
